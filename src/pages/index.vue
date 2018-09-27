f7476ff7e0106aea47f0e56566a750f265d58294
Tatsuya Kobayashi <t04.ty0425@gmail.com>
2018-09-27 23:59:21 +0900

    change index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="container">
     <div id="main">
-      <my-profile/>
-      <my-skills/>
-      <my-works/>
-      <my-contact/>
+      <!--<fade-in v-for="(item, index) in list" :key="item.id"></fade-in>-->
+        <my-profile/>
+        <my-skills/>
+        <my-works/>
+        <my-contact/>
+      <a href="#" id="return-top" v-scroll-to="'#main'">Top</a>
     </div>
   </section>
 </template>
@@ -14,13 +16,24 @@
   import MySkills from '~/components/MySkills.vue'
   import MyWorks from '~/components/MyWorks.vue'
   import MyContact from "~/components/MyContact";
+  import FadeIn from "~/components/FadeIn";
 
   export default {
     components: {
       MyContact,
       MyProfile,
       MySkills,
-      MyWorks
+      MyWorks,
+      FadeIn,
+    },
+    data: function () {
+      return {
+        list: [
+          {id: 1, name: 'Java'}, {id: 2, name: 'Spring Boot'}, {id: 3, name: 'Kotlin'}, {id: 4, name: 'JavaScript'},
+          {id: 5, name: 'Vue.js'}, {id: 6, name: 'HTML5/CSS3'}, {id: 7, name: 'SQL'}, {id: 8, name: 'AWS'},
+          {id: 10, name: 'Git'}, {id: 11, name: 'Photo'}, {id: 12, name: 'Video'}, {id: 13, name: 'Editing'},
+        ]
+      }
     }
   }
 </script>
@@ -56,5 +69,27 @@
     height: 250px;
   }
 
+  /*.fade-enter-active, .fade-leave-active {*/
+    /*transition: opacity 2s*/
+  /*}*/
+
+  /*.fade-enter, .fade-leave-to {*/
+    /*opacity: 0*/
+  /*}*/
+
+  #return-top {
+    background-color: #000;
+    border-radius: 50%;
+    bottom: 50px;
+    color: #fff;
+    font-size: 14px;
+    height: 50px;
+    line-height: 50px;
+    outline: 0;
+    position: fixed;
+    right: 50px;
+    text-align: center;
+    width: 50px;
+  }
 </style>
 
